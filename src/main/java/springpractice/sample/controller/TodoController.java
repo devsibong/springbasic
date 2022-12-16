ca03e118ca611ab586c5b4277dbd1ba686359fec
devsibong <seohae7785@gmail.com>
2022-12-16 11:04:02 +0900

    test: RequestMapping 테스트

---
+++ src/main/java/springpractice/sample/controller/TodoController.java
@@ -0,0 +1,37 @@
+package springpractice.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@RequestMapping("/todo")
+@Log4j2
+public class TodoController {
+	
+	
+	@RequestMapping("/list")
+	public void list() {
+		log.info("todo list............");
+	}
+	
+	
+//	@RequestMapping(value = "/register", method = RequestMethod.GET)
+//	public void register() {
+//		log.info("todo register.......");
+//	}
+	
+	@GetMapping("/register")
+	public void registerGET() {
+		log.info("todo register GET........");
+	}
+	
+	@PostMapping("/register")
+	public void registerPOST() {
+		log.info("todo register POST........");
+	}
+}
