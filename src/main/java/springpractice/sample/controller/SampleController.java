c18457ea54b597d16122f6f811ede8b420352208
devsibong <seohae7785@gmail.com>
2022-12-16 17:13:34 +0900

    add: Exception 처리

--- src/main/java/springpractice/sample/controller/SampleController.java
+++ src/main/java/springpractice/sample/controller/SampleController.java
@@ -3,8 +3,10 @@ package springpractice.sample.controller;
 import java.time.LocalDate;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -38,5 +40,36 @@ public class SampleController {
 		log.info("ex3...........");
 		log.info("dueDate: " + dueDate);
 		
+	}
+	
+	@GetMapping("/ex4")
+	public void ex4(Model model) {
+		log.info(".................");
+		
+		model.addAttribute("message", "Hello World");
+	}
+	
+//	@GetMapping("/ex5")
+//	public void ex5(@ModelAttribute("dto") TodoDTO todoDTO, Model model) {
+//		log.info(todoDTO);
+//	}
+	
+	@GetMapping("/ex5")
+	public String ex5(RedirectAttributes redirectAttributes) {
+		redirectAttributes.addAttribute("name", "ABC");
+		redirectAttributes.addFlashAttribute("result", "success");
+		
+		return "redirect:/ex6";
+	}
+	
+	@GetMapping("/ex6")
+	public void ex6() {
+		
+	}
+	
+	@GetMapping("/ex7")
+	public void ex7(String p1, int p2) {
+		log.info("p1.........."+p1);
+		log.info("p2.........."+p2);
 	}
 }
