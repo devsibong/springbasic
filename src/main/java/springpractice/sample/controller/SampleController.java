7b29d24c9ccbfeaf92aa609f849e09c6bbc8d4a7
devsibong <seohae7785@gmail.com>
2022-12-16 11:16:28 +0900

    test: RequestParam 테스트

--- src/main/java/springpractice/sample/controller/SampleController.java
+++ src/main/java/springpractice/sample/controller/SampleController.java
@@ -1,7 +1,10 @@
 package springpractice.sample.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -14,4 +17,26 @@ public class SampleController {
 		log.info("hello spring");
 	}
 	
+	@GetMapping("/ex1")
+	public void ex1(String name, int age) {
+		log.info("ex1..........");
+		log.info("name: " + name);
+		log.info("age: " + age);
+	}
+	
+	@GetMapping("/ex2")
+	public void ex2(@RequestParam(name= "name", defaultValue = "AAA") String name, 
+					@RequestParam(name= "age", defaultValue = "20")int age) {
+		log.info("ex2...........");
+		log.info("name: " + name);
+		log.info("age: " + age);
+		
+	}
+	
+	@GetMapping("/ex3")
+	public void ex3(LocalDate dueDate) {
+		log.info("ex3...........");
+		log.info("dueDate: " + dueDate);
+		
+	}
 }
